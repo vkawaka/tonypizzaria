bdae1639f5ca02c408ca3a01f4153bc64ad48aeb
unknown <nathaliavkawakami@gmail.com>
2023-12-10 19:48:06 -0300

    .

---
+++ js/product.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function toggleLike() {
+    // Get the checkbox element
+    var checkbox = document.getElementById('fav');
+
+    // Toggle the checked state
+    checkbox.checked = !checkbox.checked;
+  }
